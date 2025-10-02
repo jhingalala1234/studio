d9d802be4096f1553c1cb0313a726e114b71c6c8
CloudX - SRMIST <cloudx.srmist@gmail.com>
2025-10-02 12:39:02 +0000

    changed-code-files

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD-kMfX1zbsDDriMrrjHuK2RkKqeqxObvk",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "cloudx-central-dashboard.firebaseapp.com",
   projectId: "cloudx-central-dashboard",
   storageBucket: "cloudx-central-dashboard.appspot.com",
